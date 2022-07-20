44c6eaf66957758daec0671d4f79cebccfbe767f
Juergen Hoetzel <juergen@hoetzel.info>
2022-07-20 22:12:19 +0200

    python 3.10 is required

--- noxfile.py
+++ noxfile.py
@@ -43,7 +43,7 @@ def dev(session: nox.Session) -> None:
     )
 
 
-@nox.session(python=["3.8", "3.9", "3.10"])
+@nox.session(python=["3.10"])
 def test(session: nox.Session) -> None:
     """
     Runs the test suite.
