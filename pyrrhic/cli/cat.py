d540b975adee229da51dd6fa45062ed49964b33f
Juergen Hoetzel <juergen@hoetzel.info>
2022-07-23 12:22:57 +0200

    New cat command: index

--- pyrrhic/cli/cat.py
+++ pyrrhic/cli/cat.py
@@ -4,6 +4,7 @@ from pprint import pprint
 from pyrrhic.cli.state import state
 from pyrrhic.cli.util import catch_exception, get_dir_masterkey
 from pyrrhic.crypto.keys import get_config
+from pyrrhic.repo.index import get_index
 
 import typer
 
@@ -30,3 +31,15 @@ def config():
     keys_dir = os.path.join(state.repository, "keys")
     masterkey = get_dir_masterkey(keys_dir, state.password)
     pprint(get_config(masterkey, os.path.join(state.repository, "config")))
+
+
+@app.command()
+@catch_exception(ValueError, exit_code=1)
+def index(index_id: str):
+    """Return index JSON to stdout"""
+    if not state.repository:
+        raise ValueError("Please specify repository location")
+    keys_dir = os.path.join(state.repository, "keys")
+    masterkey = get_dir_masterkey(keys_dir, state.password)
+    index = get_index(os.path.join(state.repository, "index", index_id), masterkey)
+    print(index.json(indent=2))
