dfd1b1d7cf5368c85cf69f6b4fa960c18c552f10
Juergen Hoetzel <juergen@hoetzel.info>
2022-07-23 12:20:11 +0200

    Index: Use pydantic

--- pyrrhic/repo/index.py
+++ pyrrhic/repo/index.py
@@ -1,10 +1,30 @@
 import json
+from typing import List
+
+from pydantic import BaseModel
 
 from pyrrhic.crypto.keys import MasterKey, decrypt_mac
+
+
+class Blob(BaseModel):
+    id: str
+    type: str
+    offset: int
+    length: int
+
+
+class Pack(BaseModel):
+    id: str
+    blobs: List[Blob]
+
+
+class Index(BaseModel):
+    packs: List[Pack]
 
 
 def get_index(path: str, key: MasterKey) -> dict[str, dict]:
     with open(path, "rb") as f:
         bs = f.read()
         plain = decrypt_mac(key, bs)
-    return json.loads(plain)
+    json_index = json.loads(plain)
+    return Index(**json_index)
