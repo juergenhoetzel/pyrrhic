4f9c56994ba422273858c18b0a8cff8cb0054860
Jürgen Hötzel <juergen@hoetzel.info>
2022-07-26 21:52:44 +0200

    New command: cat snapshot

--- pyrrhic/cli/cat.py
+++ pyrrhic/cli/cat.py
@@ -5,6 +5,7 @@ from pyrrhic.cli.state import state
 from pyrrhic.cli.util import catch_exception, get_dir_masterkey
 from pyrrhic.crypto.keys import get_config
 from pyrrhic.repo.index import get_index
+from pyrrhic.repo.snapshot import get_snapshot
 
 import typer
 
@@ -40,3 +41,14 @@ def index(index_id: str):
     masterkey = get_dir_masterkey(keys_dir, valid_state.password)
     index = get_index(os.path.join(valid_state.repository, "index", index_id), masterkey)
     print(index.json(indent=2))
+
+
+@app.command()
+@catch_exception(ValueError, exit_code=1)
+def snapshot(snapshot_id: str):
+    """Return snapshot JSON to stdout"""
+    valid_state = state.get_valid_state()
+    keys_dir = os.path.join(valid_state.repository, "keys")
+    masterkey = get_dir_masterkey(keys_dir, valid_state.password)
+    snapshot = get_snapshot(os.path.join(valid_state.repository, "snapshots", snapshot_id), masterkey)
+    print(snapshot.json(indent=2))
