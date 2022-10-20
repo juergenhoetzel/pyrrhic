856c47c4a99aa983c5713dbf621916af282d7cc3
Juergen Hoetzel <juergen@hoetzel.info>
2022-10-20 21:29:36 +0200

    Fix possible memory error

--- pyrrhic/util.py
+++ pyrrhic/util.py
@@ -42,5 +42,7 @@ def decompress(b: bytes, uncompressed_size: int) -> bytes:
 
 def maybe_decompress(b: bytes) -> bytes:
     if b[0] == 2:  # compressed v2 format
-        return _zdec.decompress(b[1:], max_output_size=16 * 1024 * 1024 * 1024)  # from restic source code
+        return _zdec.decompress(
+            b[1:], max_output_size=2147483648
+        )  # https://stackoverflow.com/questions/69270987/how-to-resolve-the-error-related-to-frame-used-in-zstandard-which-requires-too-m
     return b
